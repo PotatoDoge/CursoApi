ad87ac375d9e98f1fef4f26c740dfeb5b745106d
Bastian Prado <bastian.prado.v@gmail.com>
2026-02-04 16:09:11 -0800

    Second commit

--- src/main/java/com/cursoapi/controlador/EstudianteControlador.java
+++ src/main/java/com/cursoapi/controlador/EstudianteControlador.java
@@ -1,4 +1,26 @@
 package com.cursoapi.controlador;
 
-public class EstudiantesControlador {
+import com.cursoapi.model.Estudiante;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/estudiantes")
+public class EstudianteControlador {
+
+    @GetMapping
+    public String getEstudiante(){
+        return "Hola estudiantes";
+    }
+
+    @PostMapping
+    public ResponseEntity<Estudiante> postEstudiante(@RequestBody Estudiante estudiante){
+        return new ResponseEntity<>(estudiante, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("{matricula}")
+    public String deleteEstudiante(@PathVariable String matricula){
+        return "Hola estudiante eliminado";
+    }
 }
--- src/main/java/com/cursoapi/model/Estudiante.java
+++ src/main/java/com/cursoapi/model/Estudiante.java
@@ -1,4 +1,48 @@
 package com.cursoapi.model;
 
 public class Estudiante {
+
+    public Estudiante(String nombre, String apellido, String matricula, Integer edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.matricula = matricula;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    private String nombre;
+    private String apellido;
+    private String matricula;
+    private Integer edad;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
 }
